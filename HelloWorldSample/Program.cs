0070ab7dd72669f9ccf18c6b45ae3856be84d518
Chang Thao <ctntpu@gmail.com>
2022-01-31 13:27:10 -0600

    added Hello world

---
+++ HelloWorldSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            Console.WriteLine("My name is Chang.");
+            Console.WriteLine("The current year is 2022.");
+            Console.ReadLine();
+        }
+    }
+}
